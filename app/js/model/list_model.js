c177f7da781c080b246241bbc1d72d4be267f71c
Michael Henretty <michael.henretty@gmail.com>
2015-01-07 15:40:23 -0800

    add hosted fxos video app to directory

--- app/js/model/list_model.js
+++ app/js/model/list_model.js
@@ -3,6 +3,12 @@ import { Model } from 'components/fxos-mvc/dist/mvc';
 export default class ListModel extends Model {
 	getAppList() {
 		return {
+			'http://fxos.github.io/camera/dist/app/manifest.webapp': {
+				manifestURL: 'http://fxos.github.io/camera/dist/app/manifest.webapp',
+				name: 'camera',
+				type: 'hosted',
+				url: 'https://github.com/fxos/camera'
+			},
 			'http://fxos.github.io/video/app/manifest.webapp': {
 				manifestURL: 'http://fxos.github.io/video/app/manifest.webapp',
 				name: 'video',
