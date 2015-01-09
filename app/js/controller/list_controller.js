c8989ee6bcde7ec3509ae5bdbb365bed177d409f
Michael Henretty <michael.henretty@gmail.com>
2015-01-08 16:06:32 -0800

    add revision data to install meta

--- app/js/controller/list_controller.js
+++ app/js/controller/list_controller.js
@@ -65,14 +65,16 @@ export default class ListController extends Controller {
 			console.log('installing hosted app, ', manifest);
 			installReq = navigator.mozApps.install(manifest, {
 				installMetaData: {
-					url: appData.url
+					url: appData.url,
+					revision: appData.revision
 				}
 			});
 		} else if (type === 'packaged') {
 			console.log('installing packaged app, ', manifest);
 			installReq = navigator.mozApps.installPackage(manifest, {
 				installMetaData: {
-					url: appData.url
+					url: appData.url,
+					revision: appData.revision
 				}
 			});
 		} else {
--- app/js/model/list_model.js
+++ app/js/model/list_model.js
@@ -7,19 +7,22 @@ export default class ListModel extends Model {
 				manifestURL: 'http://fxos.github.io/camera/dist/app/manifest.webapp',
 				name: 'camera',
 				type: 'hosted',
-				url: 'https://github.com/fxos/camera'
+				url: 'https://github.com/fxos/camera',
+				revision: '8b5a7d9061a0d0210f24ff253110a8ce935104ca'
 			},
 			'http://fxos.github.io/video/app/manifest.webapp': {
 				manifestURL: 'http://fxos.github.io/video/app/manifest.webapp',
 				name: 'video',
 				type: 'hosted',
-				url: 'https://github.com/fxos/video'
+				url: 'https://github.com/fxos/video',
+				revision: 'b3c913669bc7039e980586bd9d319a0ca2ac6003'
 			},
 			'http://henretty.us/manifest.webapp': {
 				manifestURL: 'http://henretty.us/manifest.webapp',
 				name: 'clock',
 				type: 'packaged',
-				url: 'https://github.com/mozilla-b2g/gaia/tree/apps/clock/'
+				url: 'https://github.com/mozilla-b2g/gaia/tree/apps/clock/',
+				revision: '5f0dd37917c4a6d8fa8724715d4d3797419f9013'
 			}
 		};
 	}
