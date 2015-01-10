838d3f7b01c8f179941d62eeb2be0f7450fb4cf3
Doug Sherk <github@sherk.me>
2015-01-09 20:49:53 -0500

    Add Dialer to app listing.

--- app/js/model/list_model.js
+++ app/js/model/list_model.js
@@ -17,6 +17,13 @@ export default class ListModel extends Model {
 				url: 'https://github.com/fxos/video',
 				revision: 'b3c913669bc7039e980586bd9d319a0ca2ac6003'
 			},
+			'http://fxos.github.io/dialer/app/manifest.webapp': {
+				manifestURL: 'http://fxos.github.io/dialer/app/manifest.webapp',
+				name: 'dialer',
+				type: 'hosted',
+				url: 'https://github.com/fxos/dialer',
+				revision: '835af6ccf608a51bc87711d1c40fc4f33a1fc12b'
+			},
 			'http://henretty.us/manifest.webapp': {
 				manifestURL: 'http://henretty.us/manifest.webapp',
 				name: 'clock',
