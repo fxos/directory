6bf1c6c75a308f7c07140a72888efc14d71fbe69
Michael Henretty <michael.henretty@gmail.com>
2015-03-02 17:32:38 -0800

    Make link to contribution link to github

--- app/js/view/main_view.js
+++ app/js/view/main_view.js
@@ -7,7 +7,7 @@ export default class MainView extends View {
     var string = `
       <gaia-header>
         <h1>Hackerplace</h1>
-        <a id="upload-link" href="https://github.com/fxos/directory"></a>
+        <a id="upload-link" target="_blank" href="https://github.com/fxos/directory"></a>
       </gaia-header>
       <gaia-dialog-alert id="alert-dialog">Placeholder</gaia-dialog-alert>`;
     return string;
