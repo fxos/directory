83f09beebab6026bc42ab7b503b205565cd9d3ac
Yura Zenevich <yzenevich@mozilla.com>
2015-03-30 11:19:57 -0400

    Bug 1134001 - adding support for install activity for Hackerplace.

--- app/js/controller/main_controller.js
+++ app/js/controller/main_controller.js
@@ -1,35 +1,28 @@
-import { Controller } from 'components/fxos-mvc/dist/mvc';
+import { RoutingController } from 'components/fxos-mvc/dist/mvc';
 import 'gaia-component';
 import 'gaia-dialog';
 import MainView from 'js/view/main_view';
 import ListController from 'js/controller/list_controller';
 
-export default class MainController extends Controller {
+import { ActivityHelper } from 'js/lib/helpers';
+
+export default class MainController extends RoutingController {
 
 	constructor() {
-
 		this.view = new MainView({ el: document.body });
-		this.controllers = {
-			'list': new ListController()
-		};
-		this.activeController = null;
+		this.activityHelper = new ActivityHelper();
+		this.listController = new ListController();
+		super({
+			apps: this.listController,
+			addons: this.listController
+		});
 	}
 
 	main() {
-		this.view.render();
-		window.addEventListener('hashchanged', this.route.bind(this));
-		this.route();
-		document.body.classList.remove('loading');
-	}
-
-	route() {
-		var route = window.location.hash;
-		switch (route) {
-			default:
-				this.activeController = this.controllers.list;
-				break;
-		}
-
-		this.activeController.main();
+		this.activityHelper.ready.then(route => {
+			this.view.render(this.activityHelper.isActivity);
+			window.location.hash = '#' + route;
+			document.body.classList.remove('loading');
+		});
 	}
 }
