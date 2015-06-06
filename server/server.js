d0c0f35d30a023fcf588298c9bcfc96ed517c59d
Michael Henretty <michael.henretty@gmail.com>
2015-06-05 21:48:04 -0700

    Update server to handle form data

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express=require('express');
 var path = require('path');
+var fs = require('fs');
+var ff = require('ff');
 var uuid = require('node-uuid');
 var mv = require('mv');
 
@@ -7,16 +9,34 @@ const PUBLIC_PATH = '../../public_static/';
 const PORT = 3001;
 
 function upload(req, res) {
-  var oldFilePath = req.headers['x-file'];
-  var filename = uuid.v1() + '.zip';
-  var newFilePath = path.resolve(__dirname, PUBLIC_PATH, filename);
-  mv(oldFilePath, newFilePath, function(err) {
-    if (err) {
-      console.log('move failed', err);
+  var name = req.query.name;
+  var description = req.query.description;
+  var metaContent = name + '\n' + description;
+
+  console.log('got one!', name, description);
+
+  var oldPath = req.headers['x-file'];
+  var newPath = path.resolve(__dirname, PUBLIC_PATH);
+
+  var prefix = name.replace(' ', '_') + '__' + uuid.v1();
+  var packageFilename = prefix + '.zip';
+  var metaFilename = prefix + '.txt';
+
+  var f = ff(function() {
+    mv(oldPath, path.join(newPath, packageFilename), f());
+    fs.writeFile(path.join(newPath, metaFilename), metaContent, f());
+  }, function(mvErr, writeErr) {
+    if (mvErr) {
+      console.log('move failed', mvErr);
       res.status(500).send('upload failed');
       return;
     }
-    console.log('upload complete', filename);
+    if (writeErr) {
+      console.log('move failed', writeErr);
+      res.status(500).send('upload failed');
+      return;
+    }
+    console.log('upload complete', prefix);
     res.end('upload complete');
   });
 }
